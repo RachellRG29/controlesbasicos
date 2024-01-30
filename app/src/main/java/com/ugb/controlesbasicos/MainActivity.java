43a8fffad43bc03075b2e0e2cf26fe1ecab2dd28
rachellramirez29 <RG21019.ues.edu.sv>
2024-01-29 22:36:23 -0600

    Calculadora basica falta que funcione correctamente factorial y raiz.

---
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -0,0 +1,95 @@
+package com.ugb.controlesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tempVal;
+    RadioGroup opt;
+    Button btn;
+    Spinner spn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    //opt = findViewById(R.id.optCalculadora);
+                    spn = findViewById(R.id.spnOpciones);
+
+                    tempVal = findViewById(R.id.txtnum1);
+                    double num1 = Double.parseDouble(tempVal.getText().toString());
+
+                    tempVal = findViewById(R.id.txtnum2);
+                    double num2 = Double.parseDouble(tempVal.getText().toString());
+
+                    double resp = 0;
+                        /*switch (opt.getCheckedRadioButtonId()){
+                            case R.id.optSuma:
+                                resp = num1+num2;
+                                break;
+                            case R.id.optResta:
+                                resp = num1-num2;
+                                break;
+                            case R.id.optMultiplicacion:
+                                resp=num1*num2;
+                                break;
+                            case R.id.optDivision:
+                                resp = num1/num2;
+                                break;
+                        }*/
+                    switch (spn.getSelectedItemPosition()){
+                        case 0: //suma
+                            resp = num1+num2;
+                            break;
+                        case 1: //resta
+                            resp = num1-num2;
+                            break;
+                        case 2: //multiplicacion
+                            resp = num1*num2;
+                            break;
+                        case 3: //division
+                            resp = num1/num2;
+                            break;
+                        case 4: //exponenciacion
+                            resp = Math.pow(num1,num2);
+                            break;
+                        case 5: //porcentaje
+                            resp= num1*(num2/100.0);
+                            break;
+                        case 6: //factorial
+                            resp=1;
+                                for(int i=1; i<num1; i++){
+                                    resp=resp*i;
+                                }
+                            break;
+                        case 7: //raiz
+                            if (num1==num1){
+                                resp = Math.sqrt(num1);
+                            } else if(num2==num2){
+                                resp = Math.sqrt(num2);
+                            }
+                            break;
+                    }
+                    tempVal = findViewById(R.id.lblrespuesta);
+                    tempVal.setText("Respuesta: " + resp );
+                }catch (Exception e){
+                    tempVal = findViewById(R.id.lblrespuesta);
+                    tempVal.setText("Error: "+ e.getMessage());
+                }
+            }
+        });
+
+    }
+}
